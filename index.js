2badeaaa7625253552b88a329fc660947f0a527b
Chance Arthur <hello@chancearthur.com>
2019-05-02 12:40:14 -0400

    Add new variants (#1)
    The following variants are now available:
    - dark-hover
    - dark-focus
    - dark-active
    - dark-group-hover
    - dark-focus-within

--- index.js
+++ index.js
@@ -5,5 +5,35 @@ module.exports = function() {
         return `.mode-dark .${e(`dark${separator}${className}`)}`;
       });
     });
+
+    addVariant('dark-hover', ({modifySelectors, separator}) => {
+      modifySelectors(({className}) => {
+        return `.mode-dark .${e(`dark-hover${separator}${className}`)}:hover`;
+      });
+    });
+
+    addVariant('dark-focus', ({modifySelectors, separator}) => {
+      modifySelectors(({className}) => {
+        return `.mode-dark .${e(`dark-focus${separator}${className}`)}:focus`;
+      });
+    });
+
+    addVariant('dark-active', ({modifySelectors, separator}) => {
+      modifySelectors(({className}) => {
+        return `.mode-dark .${e(`dark-active${separator}${className}`)}:active`;
+      });
+    });
+
+    addVariant('dark-group-hover', ({modifySelectors, separator}) => {
+      modifySelectors(({className}) => {
+        return `.mode-dark .group:hover .${e(`dark-group-hover${separator}${className}`)}`;
+      });
+    });
+
+    addVariant('dark-focus-within', ({modifySelectors, separator}) => {
+      modifySelectors(({className}) => {
+        return `.mode-dark .${e(`dark-focus-within${separator}${className}`)}:focus-within`;
+      });
+    });
   };
 };
