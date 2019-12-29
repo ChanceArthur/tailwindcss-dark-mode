dcc7753697a69b2453027e8b50749a6301df5f68
Thomas Betous <tbetous@gmail.com>
2019-12-29 23:07:06 +0100

    Add the ability to customize dark selector

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 module.exports = function() {
-  return function({addVariant, e}) {
+  return function({addVariant, theme, e}) {
+    const darkSelector = theme('darkSelector', '.mode-dark');
     addVariant('dark', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `.mode-dark .${e(`dark${separator}${className}`)}`;
+        return `${darkSelector} .${e(`dark${separator}${className}`)}`;
       });
     });
 
     addVariant('dark-hover', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `.mode-dark .${e(`dark-hover${separator}${className}`)}:hover`;
+        return `${darkSelector} .${e(`dark-hover${separator}${className}`)}:hover`;
       });
     });
 
     addVariant('dark-focus', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `.mode-dark .${e(`dark-focus${separator}${className}`)}:focus`;
+        return `${darkSelector} .${e(`dark-focus${separator}${className}`)}:focus`;
       });
     });
 
     addVariant('dark-active', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `.mode-dark .${e(`dark-active${separator}${className}`)}:active`;
+        return `${darkSelector} .${e(`dark-active${separator}${className}`)}:active`;
       });
     });
 
     addVariant('dark-group-hover', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `.mode-dark .group:hover .${e(`dark-group-hover${separator}${className}`)}`;
+        return `${darkSelector} .group:hover .${e(`dark-group-hover${separator}${className}`)}`;
       });
     });
 
     addVariant('dark-focus-within', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `.mode-dark .${e(`dark-focus-within${separator}${className}`)}:focus-within`;
+        return `${darkSelector} .${e(`dark-focus-within${separator}${className}`)}:focus-within`;
       });
     });
   };
