dfb3421bfc0ce76c7faf704be35e30ea6e91e009
Kasey <kculmback@users.noreply.github.com>
2020-07-27 11:24:02 -0700

    Watch for changes to dark mode (#34)

--- prefers-dark.js
+++ prefers-dark.js
@@ -2,8 +2,19 @@ function checkDarkMode() {
   return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 }
 
-if (checkDarkMode()) {
-  document.documentElement.classList.add('mode-dark');
-} else {
-  document.documentElement.classList.remove('mode-dark');
+function watchDarkMode() {
+  if (!window.matchMedia) return;
+
+  window.matchMedia('(prefers-color-scheme: dark)').addListener(addDarkModeSelector);
 }
+
+function addDarkModeSelector() {
+  if (checkDarkMode()) {
+    document.documentElement.classList.add('mode-dark');
+  } else {
+    document.documentElement.classList.remove('mode-dark');
+  }
+}
+
+addDarkModeSelector();
+watchDarkMode();
