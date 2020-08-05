89fc56ab9df87eca48340df80664faf916c963c0
subhero24 <github@bvdsoftware.be>
2020-08-05 23:16:54 +0200

    Add support for styling root element (#36)

--- index.js
+++ index.js
@@ -3,61 +3,61 @@ module.exports = function() {
     const darkSelector = theme('darkSelector', '.mode-dark');
     addVariant('dark', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `${darkSelector} .${e(`dark${separator}${className}`)}`;
+        return `${darkSelector} .${e(`dark${separator}${className}`)}, ${darkSelector}.${e(`dark${separator}${className}`)}`;
       });
     });
 
     addVariant('dark-hover', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `${darkSelector} .${e(`dark-hover${separator}${className}`)}:hover`;
+        return `${darkSelector} .${e(`dark-hover${separator}${className}`)}:hover, ${darkSelector}.${e(`dark-hover${separator}${className}`)}:hover`;
       });
     });
 
     addVariant('dark-focus', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `${darkSelector} .${e(`dark-focus${separator}${className}`)}:focus`;
+        return `${darkSelector} .${e(`dark-focus${separator}${className}`)}:focus, ${darkSelector}.${e(`dark-focus${separator}${className}`)}:focus`;
       });
     });
 
     addVariant('dark-active', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `${darkSelector} .${e(`dark-active${separator}${className}`)}:active`;
+        return `${darkSelector} .${e(`dark-active${separator}${className}`)}:active, ${darkSelector}.${e(`dark-active${separator}${className}`)}:active`;
       });
     });
 
     addVariant('dark-disabled', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `${darkSelector} .${e(`dark-disabled${separator}${className}`)}:disabled`;
+        return `${darkSelector} .${e(`dark-disabled${separator}${className}`)}:disabled, ${darkSelector}.${e(`dark-disabled${separator}${className}`)}:disabled`;
       });
     });
 
     addVariant('dark-group-hover', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `${darkSelector} .group:hover .${e(`dark-group-hover${separator}${className}`)}`;
+        return `${darkSelector} .group:hover .${e(`dark-group-hover${separator}${className}`)}, ${darkSelector}.group:hover .${e(`dark-group-hover${separator}${className}`)}`;
       });
     });
 
     addVariant('dark-focus-within', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `${darkSelector} .${e(`dark-focus-within${separator}${className}`)}:focus-within`;
+        return `${darkSelector} .${e(`dark-focus-within${separator}${className}`)}:focus-within, ${darkSelector}.${e(`dark-focus-within${separator}${className}`)}:focus-within`;
       });
     });
 
     addVariant('dark-even', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `${darkSelector} .${e(`dark-even${separator}${className}`)}:nth-child(even)`;
+        return `${darkSelector} .${e(`dark-even${separator}${className}`)}:nth-child(even), ${darkSelector}.${e(`dark-even${separator}${className}`)}:nth-child(even)`;
       });
     });
 
     addVariant('dark-odd', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `${darkSelector} .${e(`dark-odd${separator}${className}`)}:nth-child(odd)`;
+        return `${darkSelector} .${e(`dark-odd${separator}${className}`)}:nth-child(odd), ${darkSelector}.${e(`dark-odd${separator}${className}`)}:nth-child(odd)`;
       });
     });
 
     addVariant('dark-placeholder', ({modifySelectors, separator}) => {
       modifySelectors(({className}) => {
-        return `${darkSelector} .${e(`dark-placeholder${separator}${className}`)}::placeholder`;
+        return `${darkSelector} .${e(`dark-placeholder${separator}${className}`)}::placeholder, ${darkSelector}.${e(`dark-placeholder${separator}${className}`)}::placeholder`;
       });
     });
   };
